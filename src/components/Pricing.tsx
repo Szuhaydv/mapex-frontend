917cc36da1661503f68bf756ab5ec429e0264132
Szuhay Dávid <szuhaydv@gmail.com>
2024-01-26 11:06:15 +0100

    stripe integration

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -29,9 +29,11 @@ const Pricing = () => {
                 <h3>Advanced Plan</h3>
                 <p>Try out Mapex for free, upgrade when you feel comfortable!</p>
                 <h2>2 $ / year</h2>
-                <a href="https://buy.stripe.com/test_00g9B3awG8tX18s9AA">
-                    <button className="btn btn-primary">Select Plan</button>
-                </a>
+                <stripe-buy-button
+                buy-button-id="buy_btn_1OSM5NCKp5tSawCxgRU8LLbx"
+                publishable-key="pk_test_51OGGa8CKp5tSawCxZps4QKwg1sh7XqGgtiU66PbgDQt2ofykONeVNR50gJjFpf2P6kRPK49r9USh89HDNg53fmZw00osx2M9Ka"
+                >
+                </stripe-buy-button>
                 <div className="pros">
                     <div className="pro">
                         <img src="./checkmark.svg" alt="" />
@@ -51,7 +53,10 @@ const Pricing = () => {
                 <h3>VIP Plan</h3>
                 <p>Try out Mapex for free, upgrade when you feel comfortable!</p>
                 <h2>5 $ / year</h2>
-                <stripe-buy-button buy-button-id="buy_btn_1OSMDMCKp5tSawCxAcgh4DyS" publishable-key="pk_test_51OGGa8CKp5tSawCxZps4QKwg1sh7XqGgtiU66PbgDQt2ofykONeVNR50gJjFpf2P6kRPK49r9USh89HDNg53fmZw00osx2M9Ka">
+                <stripe-buy-button
+                buy-button-id="buy_btn_1OSMDMCKp5tSawCxAcgh4DyS"
+                publishable-key="pk_test_51OGGa8CKp5tSawCxZps4QKwg1sh7XqGgtiU66PbgDQt2ofykONeVNR50gJjFpf2P6kRPK49r9USh89HDNg53fmZw00osx2M9Ka"
+                >
                 </stripe-buy-button>
                 <div className="pros">
                     <div className="pro">

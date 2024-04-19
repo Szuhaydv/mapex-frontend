cf87eac83df3ab125c829cc24b20b10486092302
szuhaydv <szuhaydv@gmail.com>
2024-04-19 10:34:55 +0200

    copy icon final

--- src/components/BlogContact.tsx
+++ src/components/BlogContact.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const BlogContact = () => {
   const copyText = useRef<any>(null)
@@ -154,9 +155,7 @@ const BlogContact = () => {
                   <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                 </svg>
                 <p ref={copyText}>xezamewebsite@gmail.com</p>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-copy h6" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z"/>
-                </svg>
+                <i className="bi bi-copy"></i>
                 <div ref={successMessage} className="copy-success-message position-absolute">Copied!</div>
               </div>
               <p className="grey-text">– OR DIRECTLY –</p>
